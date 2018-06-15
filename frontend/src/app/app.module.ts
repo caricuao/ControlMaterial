27bcf6cc43d255c41fa0abe02dc2a7d8009a0687
Alejandro Rojas\ajrojas <alerojas@aconcaguabs.com>
2018-06-14 23:14:42 -0400

    Primera Subida

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MatToolbarModule,MatFormFieldModule,MatSelectModule,MatInputModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { MaterialModuleShared } from './modules/material.shared.module';
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent},
+  { path: 'edit/:id', component: EditComponent},
+  { path: 'list', component: ListComponent},
+  { path: '', redirectTo: 'list', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MaterialModuleShared
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
